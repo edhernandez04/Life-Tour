1dcf3bcb73a029f130435f77098266fd5ba4fb4b
edhernandez04 <mchh.004@gmail.com>
2020-03-20 14:22:36 -0400

    login shindigs

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './pages/Home'
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login'
 
 function App() {
   return (
     <div className="App">
       <Router>
         <NavBar />
-        <Route to="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/" component={Home} />
       </Router>
     </div>
   );
--- front_end/src/pages/Login.js
+++ front_end/src/pages/Login.js
@@ -3,19 +3,24 @@ import React from 'react';
 class Login extends React.Component {
   render() {
     return (
-      <form>
-        <h1>Login</h1>
-        <div>
-          <input type="text" name="username" placeholder="Username" />
-          <label htmlFor="username">Username</label>
-        </div>
-        <div>
-          <input type="password" name="password" placeholder="Password" />
-          <label htmlFor="password">Password</label>
-        </div>
-        <input type="submit" value="Login" />
-      </form>
-    );
+    <div className="fullscreen-video-wrap">
+        <video src="https://www.videvo.net/videvo_files/converted/2013_12/preview/GLASS_BACKGROUND_CLUB.mov73294.webm" autoPlay="true" loop="true"></video>
+    <div className="formContainer">
+        <form>
+            <h1>Login</h1>
+            <div>
+                <input type="text" name="username" placeholder="Username" />
+                <label htmlFor="username">Username</label>
+            </div>
+            <div>
+                <input type="password" name="password" placeholder="Password" />
+                <label htmlFor="password">Password</label>
+            </div>
+            <input type="submit" value="Login" />
+        </form>
+    </div>
+    </div>
+    )
   }
 }
  
