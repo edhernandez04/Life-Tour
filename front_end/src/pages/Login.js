d39e51c96d709b3001ca936b2fea4feba3563c4a
edhernandez04 <mchh.004@gmail.com>
2020-03-20 15:36:37 -0400

    she looking good

--- front_end/src/pages/Login.js
+++ front_end/src/pages/Login.js
@@ -4,20 +4,17 @@ class Login extends React.Component {
   render() {
     return (
     <div className="formContainer">
-    <div className="fullscreen-video-wrap">
-        <video src="https://www.videvo.net/videvo_files/converted/2013_12/preview/GLASS_BACKGROUND_CLUB.mov73294.webm" autoPlay="true" loop="true"></video>
-        <form>
-            <h1>Login</h1>
+                <form className="actualForm">
             <div>
                 <input type="text" name="username" placeholder="Username" />
-                <label htmlFor="username">Username</label>
             </div>
             <div>
                 <input type="password" name="password" placeholder="Password" />
-                <label htmlFor="password">Password</label>
             </div>
-            <input type="submit" value="Login" />
+            <button type="submit" value="Login"> Login </button>
         </form>
+        <div className="fullscreen-video-wrap">
+        <video src="https://www.videvo.net/videvo_files/converted/2013_12/preview/GLASS_BACKGROUND_CLUB.mov73294.webm" autoPlay="true" loop="true"></video>
     </div>
     </div>
     )
--- front_end/src/pages/SignUp.js
+++ front_end/src/pages/SignUp.js
@@ -3,26 +3,28 @@ import React from 'react';
 class SignUp extends React.Component {
   render() {
     return (
-      <form>
-        <h1>SignUp</h1>
+        <div className="formContainer">
+        <form className="actualForm">
+        <h3>Get Access</h3>
         <div>
           <input type="text" name="username" placeholder="Username" />
-          <label htmlFor="username">Username</label>
         </div>
         <div>
           <input type="integer" name="age" placeholder="age" />
-          <label htmlFor="age">Age</label>
+
         </div>
         <div>
           <input type="text" name="summary" placeholder="Summary" />
-          <label htmlFor="summary">Summary</label>
         </div>
         <div>
           <input type="password" name="password" placeholder="Password" />
-          <label htmlFor="password">Password</label>
         </div>
         <input type="submit" value="SignUp" />
       </form>
+      <div className="fullscreen-video-wrap">
+        <video src="https://www.videvo.net/videvo_files/converted/2013_12/preview/GIRL_ON_SHOULDERS.mov69900.webm" autoPlay="true" loop="true"></video>
+    </div>
+    </div>
     );
   }
 }
