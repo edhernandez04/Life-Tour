b0b0c661e5ab9f69313f38f86654864f512a0e0c
edhernandez04 <mchh.004@gmail.com>
2020-03-22 13:14:23 -0400

    clean up

--- front_end/src/pages/Login.js
+++ front_end/src/pages/Login.js
@@ -5,42 +5,37 @@ class Login extends React.Component {
 
   state = {
     allUsers: [],
-    loggedIn: [],
+    loggedIn: {}
   }
 
   componentDidMount(){
-    fetch("http://localhost:3000/users")
-      .then(resp => resp.json())
-      .then(allUsers => this.setState({allUsers}))
+    fetch("http://localhost:3000/users").then(resp => resp.json()).then(allUsers => this.setState({allUsers}))
   }
 
   handleLogIn = (e) => {
     e.preventDefault()
     let foundUser = this.state.allUsers.find(user => e.target[0].value === user.name) 
-    if (foundUser) {
-        this.setState({loggedIn: foundUser})
-    } else {
-        alert("USER NOT FOUND")
-      }
+    foundUser ? this.setState({loggedIn: foundUser}) : alert("User Not Found")
+    console.log(foundUser)
     console.log(this.state.loggedIn)
   }
 
   render() {
     return (
-    <div className="formContainer">
-                <form className="actualForm" onSubmit={this.handleLogIn}>
-            <div>
-                <input type="text" name="username" placeholder="Username" />
-            </div>
-            <div>
-                <input type="password" name="password" placeholder="Password" />
-            </div>
-            <button type="submit" value="Login"> Login </button>
+      <div className="formContainer">
+        <form className="actualForm" onSubmit={this.handleLogIn}>
+          <div>
+            <input type="text" name="username" placeholder="Username" />
+          </div>
+          <div>
+            <input type="password" name="password" placeholder="Password" />
+          </div>
+          <button type="submit" value="Login"> Login </button>
         </form>
-        <div className="fullscreen-video-wrap">
+      <div className="fullscreen-video-wrap">
         <video src="https://www.videvo.net/videvo_files/converted/2013_12/preview/GLASS_BACKGROUND_CLUB.mov73294.webm" autoPlay="true" loop="true"></video>
-    </div>
-    </div>
+      </div>
+      </div>
     )
   }
 }
