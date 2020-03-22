f1769f6813489e50d8d62da3492f896c6aa1627e
Steven Zhu <stevenzhu03@gmail.com>
2020-03-22 13:37:11 -0400

    styling

--- front_end/src/pages/Login.js
+++ front_end/src/pages/Login.js
@@ -3,23 +3,25 @@ import React from 'react';
 class Login extends React.Component {
   render() {
     return (
-    <div className="formContainer">
-    <div className="fullscreen-video-wrap">
-        <video src="https://www.videvo.net/videvo_files/converted/2013_12/preview/GLASS_BACKGROUND_CLUB.mov73294.webm" autoPlay="true" loop="true"></video>
-        <form>
-            <h1>Login</h1>
-            <div>
-                <input type="text" name="username" placeholder="Username" />
-                <label htmlFor="username">Username</label>
-            </div>
-            <div>
-                <input type="password" name="password" placeholder="Password" />
-                <label htmlFor="password">Password</label>
-            </div>
-            <input type="submit" value="Login" />
-        </form>
-    </div>
-    </div>
+      <div className="login">
+        <div className="fullscreen-video-wrap">
+            <video src="https://www.videvo.net/videvo_files/converted/2013_12/preview/GLASS_BACKGROUND_CLUB.mov73294.webm" autoPlay="true" loop="true"></video>
+        </div>
+        <div className="formContainer">
+            <form>
+                <h1>Login</h1>
+                <div>
+                    <input type="text" name="username" placeholder="Username" />
+                    <label htmlFor="username">Username</label>
+                </div>
+                <div>
+                    <input type="password" name="password" placeholder="Password" />
+                    <label htmlFor="password">Password</label>
+                </div>
+                <input type="submit" value="Login" />
+            </form>
+        </div>
+      </div>
     )
   }
 }
