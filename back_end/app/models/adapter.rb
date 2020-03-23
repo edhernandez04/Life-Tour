78261f6d7ada2bf9577455873cdcc6f7f16b2b36
Steven Zhu <stevenzhu03@gmail.com>
2020-03-23 00:31:04 -0400

    added adapter class

---
+++ back_end/app/models/adapter.rb
@@ -0,0 +1,42 @@
+require "json"
+require "http"
+require "optparse"
+
+class Adapter
+
+ # Consumer Secret
+    
+ # Constants, do not change these
+ API_HOST = "https://app.ticketmaster.com"
+ EVENTS_PATH = "/discovery/v2/events"
+ SEARCH_LIMIT = 8
+
+    def self.search(keyword, location)
+     url = "#{API_HOST}#{EVENTS_PATH}"
+
+     params = {
+       keyword: keyword,
+       location: location,
+       limit: SEARCH_LIMIT,
+     }
+     response = HTTP.auth("Bearer #{API_KEY}").get(url, params: params)
+     response.parse
+
+  
+    end
+
+    def self.business_reviews(business_id)
+    url = "#{API_HOST}#{BUSINESS_PATH}#{business_id}/reviews"
+    response = HTTP.auth("Bearer #{API_KEY}").get(url)
+    response.parse["reviews"]
+    end
+    
+    def self.business(business_id)
+    url = "#{API_HOST}#{BUSINESS_PATH}#{business_id}"
+    response = HTTP.auth("Bearer #{API_KEY}").get(url)
+    response.parse
+    end
+    
+
+end
+
