2513e2f38ad615886fe33b49c596c4d0eda5156f
edhernandez04 <mchh.004@gmail.com>
2020-03-23 22:43:56 -0400

    first draft profile page

--- front_end/src/components/UserProfile.js
+++ front_end/src/components/UserProfile.js
@@ -7,27 +7,45 @@ export default class UserProfile extends React.Component{
         console.log(this.props.user)
         return (
             <div className="page">
-                <div className="col 4">
-                <div className="card-userAvatar">
-                    <img src="https://i7.pngguru.com/preview/178/419/741/computer-icons-avatar-login-user-avatar.jpg" />
+                <div className="row">
+                    <div className="card-userAvatar">
+                        <img src="https://i7.pngguru.com/preview/178/419/741/computer-icons-avatar-login-user-avatar.jpg" />
+                    </div> 
+                    <div className="container-userEvents">
+                        {/* <Event /> would be individual event cards*/}
+                        <img src="https://i.pinimg.com/originals/91/b5/96/91b596bf1cff50b55fe1e312edba424a.jpg"/>
+                        <img src="https://d2z11snniwyi52.cloudfront.net/images/template/2729/23/Comedy-Retro-Microphone-Event-Ticket__front.png"/>
+                        <img src='https://d2z11snniwyi52.cloudfront.net/images/template/2770/16/Lions-Club-International-Water-Color-Event-Ticket__front.png'/>
+                        <img src="https://3.bp.blogspot.com/-c9UVJi0o2xo/UDZYX3J2qVI/AAAAAAAAA_g/cl7ZNwS5Pn8/s1600/bieber+ticket+front+side.jpg"/>
+                    </div>
                 </div>
-                <div className="card-userInfo">
-                    <h3>{this.props.user.username}</h3>
-                    <p>Name: {this.props.user.name ? this.props.user.name:"Didnt leave a name"}</p>
-                    <p>Age: {this.props.user.age ? this.props.user.age:"How old are you?"}</p>
-                    <p>Bio: {this.props.user.summary ? this.props.user.summary:"You need to update your profile"}</p>
+                <div className="row">
+                    <div className="container-otherEvents">
+                        <img src="https://fcclainc.org/sites/default/files/star-events.jpg" />
+                        <img src="https://www.visitbritain.com/sites/default/files/styles/vbmc_content_page/public/vbmc_consumer/main_images/vb34168897.jpg?itok=BIR_JBsL" />
+                        <img src="https://specials-images.forbesimg.com/imageserve/1146473379/960x0.jpg?fit=scale" />
+                        <img src="https://orbis.imgix.net/https%3A%2F%2Fs3-eu-west-1.amazonaws.com%2Fgb.files.orbis.epevaluation.co.uk%2Fimages%2FFundraising%2FOrbis-0014-P5-0428.JPG%3Fmtime%3D20181105155413?auto=compress&crop=focalpoint&fit=crop&fp-x=0&fp-y=0.5&h=660&ixlib=php-1.1.0&q=75&w=768&s=10d7fb4579ac0bb76125263be5d5a016" />
+                        <img src="https://i.insider.com/5e5d4b1efee23d47fc1f7be5?width=1100&format=jpeg&auto=webp" />
+                        <img src="https://static.mommypoppins.com/styles/image620x420/s3/lead_vessel_exterior_jmm.jpg" />
+                        <img src="https://cdn.thecrazytourist.com/wp-content/uploads/2019/04/ccimage-shutterstock_615200000.jpg" />
+                        <img src="https://static.mommypoppins.com/styles/image620x420/s3/luna_park_05_alex_lopez_0.jpg" />
+                        <img src="https://assets3.thrillist.com/v1/image/2679747/size/tmg-article_default_mobile.jpg" />
+                        <img src="https://media.cntraveler.com/photos/5851cb6b053d277e273da5ab/master/w_2048,h_1536,c_limit/best-bars-NYC-Maison-Premiere-2016.jpg" />
+                        <img src="https://thenypost.files.wordpress.com/2019/06/outdoor-rooftop-crown-bowery-1a.jpg?quality=80&strip=all&w=978&h=652" />
+                        <img src="https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fwp-content%2Fblogs.dir%2F6%2Ffiles%2F2019%2F07%2Fbest-bubble-tea-nyc-01.jpg?q=75&w=800&cbr=1&fit=max" />
+                    </div>
+                    <div className="card-userInfo">
+                        <h3>{this.props.user.username}</h3>
+                        <p>{this.props.user.name ? this.props.user.name:"Didnt leave a name"}</p>
+                        <p>{this.props.user.age ? this.props.user.age:"How old are you?"}</p>
+                        <p>{this.props.user.summary ? this.props.user.summary:"You need to update your profile"}</p>
+                        <p>Featured Friend</p>
+                        <p>dates for next tours</p>
+                        <p>most commented tours</p>
+                    </div>
                 </div>
-                </div>
-                <div className="col 8">
-                <div className="container-userEvents">
-                    {/* <Event /> would be individual event cards*/}
-                    <img src="https://i.pinimg.com/originals/91/b5/96/91b596bf1cff50b55fe1e312edba424a.jpg"/>
-                    <img src="https://d2z11snniwyi52.cloudfront.net/images/template/2729/23/Comedy-Retro-Microphone-Event-Ticket__front.png"/>
-                    <img src='https://d2z11snniwyi52.cloudfront.net/images/template/2770/16/Lions-Club-International-Water-Color-Event-Ticket__front.png'/>
-                    <img src="https://3.bp.blogspot.com/-c9UVJi0o2xo/UDZYX3J2qVI/AAAAAAAAA_g/cl7ZNwS5Pn8/s1600/bieber+ticket+front+side.jpg"/>
-                </div>
-                <div className="container-otherEvents">
-                </div>
+                <div className="fullscreen-video-wrap">
+                    <video src="https://www.videvo.net/videvo_files/converted/2014_05/preview/Flashing_Club_Scene.mov87931.webm" autoPlay={true} loop={true}></video>
                 </div>
             </div>
         )
